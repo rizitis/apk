e6829986983ca339d57a92834572f73b4d088e8a
Anagnostakis Ioannis <elahistos@yahoo.gr>
2025-02-04 22:45:44 +0200

    update
    Signed-off-by: Anagnostakis Ioannis <elahistos@yahoo.gr>

--- py3-changelog.py
+++ py3-changelog.py
@@ -15,31 +15,26 @@ def compare_package_versions(old_file, new_file, changelog_file='ChangeLog.txt')
     # Prepare the output for the changelog
     changelog_entries = []
 
-    def extract_version(pkg):
-        """Extract the version components from a package string."""
-        parts = pkg.split('-')
-        return parts if len(parts) >= 3 else []  # Return empty list if not in expected format
-
     # Loop through each new package to classify it
     for new_package in new_packages:
+        # Skip lines with timestamps
+        if new_package.startswith("#"):
+            continue
+
         if new_package not in old_set:
             # If the package is in the new file but not in the old file, it was added
             changelog_entries.append(f"Added>{new_package} :")
         else:
             # If the package exists in both files, check version
             old_version = [pkg for pkg in old_packages if pkg.split('-')[0] == new_package.split('-')[0]]
-            new_version = extract_version(new_package)
-
-            if old_version:
-                old_version = extract_version(old_version[0])
-
-            if old_version and new_version:
-                if old_version[1] != new_version[1]:
-                    # If the version numbers are different, it has been upgraded
-                    changelog_entries.append(f"Upgraded>{new_package} :")
-                elif old_version[2] != new_version[2]:
-                    # If the release numbers are different, it has been rebuilt
-                    changelog_entries.append(f"Rebuilt>{new_package} :")
+            new_version = new_package.split('-')
+            old_version = old_version[0].split('-') if old_version else []
+            if old_version and new_version and old_version[1] != new_version[1]:
+                # If the version numbers are different, it has been upgraded
+                changelog_entries.append(f"Upgraded>{new_package} :")
+            elif old_version and new_version and old_version[2] != new_version[2]:
+                # If the release numbers are different, it has been rebuilt
+                changelog_entries.append(f"Rebuilt>{new_package} :")
 
     # Check for removed packages (those in the old file but not in the new file)
     for old_package in old_packages:
